e01848c27867b18600abaa3fe9407e5b458e1adb
szjung-test <93111772+szjung-test@users.noreply.github.com>
2022-11-22 09:15:59 +0900

    Create IWeight.py

---
+++ src/detect/IWeight.py
@@ -0,0 +1,10 @@
+from threading import Lock
+from abc import *
+
+class IWeight(metaclass=ABCMeta):
+  @abstractmethod
+  def inference(self, im, size):
+    pass
+  @abstractmethod
+  def preprocess(self, im):
+    pass
