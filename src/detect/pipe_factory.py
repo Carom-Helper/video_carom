06af3d64088a6a17bdda5117c3bd99044e20b4cf
szjung-test <93111772+szjung-test@users.noreply.github.com>
2022-11-22 09:57:59 +0900

    Update pipe_factory.py

--- src/detect/pipe_factory.py
+++ src/detect/pipe_factory.py
@@ -22,23 +22,41 @@ if str(tmp) not in sys.path and os.path.isabs(tmp):
     
 # ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
 
-from pipe_cls import On2onvertToxywhPipe, PipeResource, ResourceBag, SplitCls, SplitIdx
-from DetectObjectPipe import DetectObjectPipe
-from DetectIndexPipe import DetectIndexPipe, ForceSetIdPipe
-from CheckDetectPipe import CheckDetectPipe
-from FindEdgePipe import FindEdgePipe
+from pipe_cls import One2OnePipe, ConvertToxywhPipe, IObserverPipe, ResourceBag, SplitCls, ResourceBag
+from Singleton import Singleton
+from projectionPipe import ProjectionCoordPipe
+from DetectObjectPipe import DetectObjectPipe #NPU_YOLO_DIR, CPU_YOLO_DIR
+from detect_utils import (PipeResource, LoadImages, is_test, cv2, print_args)
 
-def pipe_factory(start_pipe=None, device='cpu', display = True):
+def is_test_factory()->bool:
+     return False and is_test()
+
+def test_print(s, s1="", s2="", s3="", s4="", s5="", end="\n"):
+     if is_test_factory():
+          print("factory pipe test : ", s, s1, s2, s3, s4, s5, end=end)
+          
+def PipeFactory(metaclass=Singleton):
+     def __init__(self, start_pipe=None, device='cpu', display=True, inDB=True):
+          self.pipe, _ = pipe_factory(start_pipe=start_pipe, device=device, display=display, inDB=inDB)
+
+def pipe_factory(start_pipe=None, device='cpu', display = True, inDB=True):
     if display:
         print("initialize weights")
     #detect class and split class
     detect_cls_pipe = DetectObjectPipe(device=device, display=display)
+    xyxy2xywh = ConvertToxywhPipe()
     split_cls_pipe = SplitCls()
-    find_edge_pipe = FindEdgePipe()
+    edge_bag = ResourceBag()
+    projection_coord_pipe = ProjectionCoordPipe(display=display)
+ 
     
     # - connect
     detect_cls_pipe.connect_pipe(split_cls_pipe)     #detect class - split_cls
-    _ = split_cls_pipe.connect_pipe(find_edge_pipe) # split class - edge bag
+    xyxy2xywh.connect_pipe(projection_coord_pipe)    #detect class - split_cls
+    projection_coord_pipe.connect_pipe(split_cls_pipe)
+    _ = split_cls_pipe.connect_pipe(edge_pipe) # split class - edge bag
+    
+     
     test_print("connect edge pipe : ", _)        
     
     
@@ -52,22 +70,17 @@ def pipe_factory(start_pipe=None, device='cpu', display = True):
     
     
     #connect
-    _ = split_cls_pipe.connect_pipe(start_cutter_pipe)      # split cls - ball - start cut
-    test_print("connect sort pipe : ", _)
-    start_cutter_pipe.connect_pipe(check_detect_pipe)       # start cut - check -detect
-    check_detect_pipe.connect_pipe(xywh_pipe)               # check detect - xyxy
-    xywh_pipe.connect_pipe(repeat_pipe)                     # check detect - repeat
-    repeat_pipe.connect_pipe(detect_idx_pipe)               # repeat - detect idx
-    detect_idx_pipe.connect_pipe(force_setid_pipe)          # detect idx - force set id
-    
-    split_idx_pipe = SplitIdx()
-    ball_list = []
-    # ball bag create and connect
-    for i in range(split_idx_pipe.idx2label.__len__()):
-        bag = ResourceBag()
-        split_idx_pipe.connect_pipe(bag)            # split idx - ball bag (iterate)
-        ball_list.append(bag)
-    force_setid_pipe.connect_pipe(split_idx_pipe)    # force set id - split idx
+    detect_cls_pipe.connect_pipe(xyxy2xywh)
+    xyxy2xywh.connect_pipe(projection_coord_pipe)
+    projection_coord_pipe.connect_pipe(split_cls_pipe)
+    _ = split_cls_pipe.connect_pipe(edge_bag)
+
+    test_print("connect edge pipe : ", _)
+     
+    ball_bag = SaveBallCoordPipe(display=display) id inDB else ResourceOne()
+    _ = split_cls_pie.connect_pipe(ball_bag)
+    test_print("connect ball pipe : ", _)
+
     
     #set start_pipe end_pipe
     if start_pipe is None:
@@ -76,67 +89,66 @@ def pipe_factory(start_pipe=None, device='cpu', display = True):
         start_pipe.connect_pipe(detect_cls_pipe)
     else:
         raise TypeError("TypeError in pipe_factory")
-    return (start_pipe, ball_list,  find_edge_pipe)
+    return start_pipe, ball_bag
 
 
-
-def detect(src, device='cpu', MIN_DETS= 10, display=True):
-     ### Dataloader ###
-    source = src
-    imgsz = (640, 640)
-    pt = True
-    stride = 32
-    nr_sources = 1
-
-    dataset = LoadImages(source, img_size=imgsz, stride=stride, auto=pt)
-    vid_path, vid_writer, txt_path = [
-        None] * nr_sources, [None] * nr_sources, [None] * nr_sources
-
-    # set pipe
-    pipe, ball_bags, edge_bag = pipe_factory(device=device, display=display)
+def detect(src, device='cpu', MIN_DETS= 10, display=False, inDB=False):
+    # # set pipe
+    pipe, ball_bag = pipe_factory(device=device, display=display, inDB=inDB)
     
+    ### Dataloader ###
+    dataset = LoadImages(src)
     ### 실행 ###
-    for frame_idx, (path, im, im0s, vid_cap, s) in enumerate(dataset):
-        input = PipeResource(f_num=frame_idx, path=path,
-                             im=im, im0s=im0s, vid_cap=vid_cap, s=s)
+    for im0, path, s in dataset:
+        #point 위치 확인
+        points = [[549,109],[942,111],[1270,580],[180,565]]
+        pts = np.zeros((4, 2), dtype=np.float32)
+        for i in range(4):
+            pts[i] = points[i]
+        
+        sm = pts.sum(axis=1)  # 4쌍의 좌표 각각 x+y 계산
+        diff = np.diff(pts, axis=1)  # 4쌍의 좌표 각각 x-y 계산
+
+        topLeft = pts[np.argmin(sm)]  # x+y가 가장 값이 좌상단 좌표
+        bottomRight = pts[np.argmax(sm)]  # x+y가 가장 큰 값이 우하단 좌표
+        topRight = pts[np.argmin(diff)]  # x-y가 가장 작은 것이 우상단 좌표
+        bottomLeft = pts[np.argmax(diff)]  # x-y가 가장 큰 값이 좌하단 좌표
+        test_print(f'topLeft({type(topLeft)}):{topLeft} | ({type(bottomRight)}):{bottomRight} | ({type(topRight)}):{topRight} | ({type(bottomLeft)}):{bottomLeft}')
+        
+
+        metadata = {"path": path, "carom_id":1, "TL":topLeft, "BR":bottomRight, "TR":topRight, "BL":bottomLeft}
+        images = {"origin":im0}
+        input = PipeResource(im=im0, metadata=metadata, images=images, s=s)
         pipe.push_src(input)
+        # 원본 정사영 영역 표시
+        if display:
+            origin = input.images["origin"].copy()
+            for i in range(4):
+                origin = cv2.line(origin, (pts[i][0], pts[i][1]), (pts[(i+1)%4][0], pts[(i+1)%4][1]), (0, 255, 0), 2)
+            cv2.imshow("origin", origin)
+            cv2.waitKey(3000)
+    return ball_bag
     
-    ball_bag_list = []
-    
-    # cut index
-    for i, bag in enumerate(reversed(ball_bags)):
-        cnt = 0
-        for resource in bag.src_list:
-            # counting det num
-            if resource.__len__() > 0 :
-                resource.print()
-                cnt += 1
-                test_print(f"cnt({cnt})")
-        if cnt > MIN_DETS : 
-            test_print(f"{i} bag cnt({cnt})")
-            ball_bag_list.append(bag)
-    
-    return (ball_bag_list, edge_bag)
-    
-def test(src, device):
-    ball_bag_list, edge_bag = detect(src, device)
+def test(src = CAROM_BASE_DIR / "data", 
+         device = '0'
+         display = True):
+         ball_bag = detect(src, device, display=display)
      
-    title = "test"
-    for ball_bag in ball_bag_list:
-        for ball_det in ball_bag.src_list:
-            ball_det.imshow(title, idx_names=["1","2","3","4","5","6"], hide_labels=False)
-            cv2.waitKey(0)
-    print("edge : ", edge_bag.get_edge())
-    
+         title = "test"
+         ball_bag.print()
+     
+     
 def runner(args):
+    print_args(vars(args))
     test(args.src, args.device)
     #run(args.src, args.device)
-    detect(args.src, args.device)
+    #detect(args.src, args.device)
     
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--src', default="/../../data/videos/kj_cud_272.mp4")
     parser.add_argument('--device', default='0', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+    parser.add_argument('--display', action='store_true')
     args = parser.parse_args()
     print(args.src)
     runner(args) 
